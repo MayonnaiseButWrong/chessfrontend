8dff3fbf955843c34ff425defa7d2ca2611f9944
MayonnaiseButWrong <manaschopade@gmail.com>
2023-01-22 13:41:46 +0000

    changed the layout of the OptionsPage chess page kinda still brokem >:V

--- src/Chess.js
+++ src/Chess.js
@@ -20,21 +20,30 @@ const ChessFrontEnd = () => {
     <div className="ChessFrontEnd">
         <h1>Chess</h1>
         <div class="grid-container1">
-            <div>1</div>
-            <div>2</div>
-                <div class="grid-container2">
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
-                    <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                <div class='chessboard'>
+                    <div class="grid-container2">
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                        <div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div><div>11</div>
+                    </div>
                 </div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
+                <div class='last moves'>
+                    <h2 class='last moves header'>Last Moves</h2>
+                    <text class='last moves text'>efwasdf</text>
+                </div>
+                <div class='black pieces taken'>
+                    <h2 class='black pieces taken header'>Black Pieces Taken</h2>
+                    <text class='black pieces taken text'>dfbgdfvyjh</text>
+                </div>
+                <div class='white pieces taken'>
+                    <h2 class='white pieces taken header'>White Pieces</h2>
+                    <text class='white pieces taken text'>sghnyjukyjt</text>
+                </div>
         </div>
     </div>
     )
--- src/OptionsPage.js
+++ src/OptionsPage.js
@@ -28,24 +28,26 @@ const OptionsPage = () => {
     return (
       <div className="OptionsPage">
         <h1>Chess</h1>
-        <Link to= "/ChessGame"><div className="container"><button id="button 1"> Daily Chess</button></div>    </Link>
         <div class="grid-container">
-          <Link to= "/ChessGame"> <button id="button 2"> 2 Player Normal Chess</button>                      </Link>
-          <Link to= "/ChessGame"> <button id="button 3"> 1 Player Normal Chess</button>                      </Link>  
-          <Link to= "/ChessGame"> <button id="button 4"> 2 Player Fischer Random Chess</button>              </Link>
-          <Link to= "/ChessGame"> <button id="button 5"> 1 Player Fischer Random Chess</button>              </Link>
-          <Link to= "/ChessGame"> <button id="button 6"> 2 Player Transcendental Chess</button>              </Link>
-          <Link to= "/ChessGame"> <button id="button 7"> 1 Player Transcendental Chess</button>              </Link>
-          <Link to= "/ChessGame"> <button id="button 8"> 2 Player Upside Down Chess</button>                 </Link>
-          <Link to= "/ChessGame"> <button id="button 9"> 1 Player Upside Down Chess</button>                 </Link>  
-          <Link to= "/ChessGame"> <button id="button 10"> 2 Player Weak! Chess</button>                      </Link>
-          <Link to= "/ChessGame"> <button id="button 11"> 1 Player Weak! Chess</button>                      </Link>
-          <Link to= "/ChessGame"> <button id="button 12"> 2 Player Charge Of The Light Brigade Chess</button></Link> 
-          <Link to= "/ChessGame"> <button id="button 13"> 1 Player Charge Of The Light Brigade Chess</button></Link>
-          <Link to= "/ChessGame"> <button id="button 14"> 2 Player Peasants' Revolt Chess</button>           </Link>
-          <Link to= "/ChessGame"> <button id="button 15"> 1 Player Peasants' Revolt Chess</button>           </Link> 
-          <Link to= "/ChessGame"> <button id="button 16"> 2 Player EndGame Chess</button>                    </Link>
-          <Link to= "/ChessGame"> <button id="button 17"> 1 Player EndGame Chess</button>                    </Link>   
+          <div class= 'boarderleft'></div>
+          <div class='topbutton'><Link to= "/ChessGame"><button id="button 1"> Daily Chess</button>              </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 2"> 2 Player Normal Chess</button>                      </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 3"> 1 Player Normal Chess</button>                      </Link></div>  
+          <div><Link to= "/ChessGame"> <button id="button 4"> 2 Player Fischer Random Chess</button>              </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 5"> 1 Player Fischer Random Chess</button>              </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 6"> 2 Player Transcendental Chess</button>              </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 7"> 1 Player Transcendental Chess</button>              </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 8"> 2 Player Upside Down Chess</button>                 </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 9"> 1 Player Upside Down Chess</button>                 </Link></div>  
+          <div><Link to= "/ChessGame"> <button id="button 10"> 2 Player Weak! Chess</button>                      </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 11"> 1 Player Weak! Chess</button>                      </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 12"> 2 Player Charge Of The Light Brigade Chess</button></Link></div> 
+          <div><Link to= "/ChessGame"> <button id="button 13"> 1 Player Charge Of The Light Brigade Chess</button></Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 14"> 2 Player Peasants' Revolt Chess</button>           </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 15"> 1 Player Peasants' Revolt Chess</button>           </Link></div> 
+          <div><Link to= "/ChessGame"> <button id="button 16"> 2 Player EndGame Chess</button>                    </Link></div>
+          <div><Link to= "/ChessGame"> <button id="button 17"> 1 Player EndGame Chess</button>                    </Link></div>   
+          <div class= 'boarderright'></div>
         </div>
       </div>
     );
